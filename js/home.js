eed07611f4a2b89f58239a34649e674e298dec9a
AgustinaCasiello <aguscasiello1@gmail.com>
2020-11-14 18:39:38 -0300

    sigo trabajando en el home

--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function(){
     let populares = document.querySelector('#populares')
+    let valoradas = document.getElementById('valoradas')
+    let spopulares = document.getElementById('spopulares')
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
     .then(respuesta =>{
         return respuesta.json()
@@ -9,5 +11,25 @@ window.addEventListener('load', function(){
             populares.innerHTML += `<li> <div class="uk-panel"> <img src="https://image.tmdb.org/t/p/w500/${peliculasPopulares.results[i].poster_path} " alt="${peliculasPopulares.results[i].title}"> <div class="uk-position-center  uk-panel"><h1> ${i+1} </h1></div> </div> </li> `
         }
     })
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
+    .then(respuesta =>{
+        return respuesta.json()
+    })
+    .then(function(peliculasValoradas){
+        for(let i = 0 ; i < peliculasValoradas.results.length; i++){
+            valoradas.innerHTML += `<li> <div class="uk-panel"> <img src="https://image.tmdb.org/t/p/w500/${peliculasValoradas.results[i].poster_path} " alt="${peliculasValoradas.results[i].title}"> <div class="uk-position-center  uk-panel"><h1> ${i+1} </h1></div> </div> </li> `
+        }
+    })
+    fetch('https://api.themoviedb.org/3/tv/popular?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
+    .then(respuesta =>{
+        return respuesta.json()
+    })
+    .then(function(serieSpopulares){
+        for(let i = 0 ; i < serieSpopulares.results.length; i++){
+            spopulares.innerHTML += `<li> <div class="uk-panel"> <img src="https://image.tmdb.org/t/p/w500/${serieSpopulares.results[i].poster_path} " alt="${serieSpopulares.results[i].title}"> <div class="uk-position-center  uk-panel"><h1> ${i+1} </h1></div> </div> </li> `
+        }
+    })
+
+
 })
-// `` < >
+// `` < > 18581b65b3e6ad002984aa4952878117
