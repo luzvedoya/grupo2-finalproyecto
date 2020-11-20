27235cca5b36e9adf056659dccf570127b472155
Martina <mtomlianovich@udesa.edu.ar>
2020-11-20 14:57:03 -0300

    Segui probando con generos

--- js/generos.js
+++ js/generos.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function(){
         console.log(generos);
         generos.genres.forEach(genero => {
             listaGeneros.innerHTML += 
-            `<a href="GenresDetail.html">
+            `<a href="GenresDetail.html" + generos>
                     <article class="todos">
                         <h2 class="palabra"> ${genero.name} </h2>
                     </article>
