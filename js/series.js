e65561d010134b6141be1f783d70cf6ed91d659d
Luz Vedoya <lvedoya@udesa.edu.ar>
2020-11-22 20:09:47 -0300

    avance con detalle de series

--- js/series.js
+++ js/series.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function(){
     let seriesPopulares = document.querySelector('.seriesPopulares');
-    let cadaSeriePopular = document.querySelector('.cadaSeriePopular')
+    let cadaSeriePopular = document.querySelector('.cadaSeriePopular');
+    let aclamadasPor = document.querySelector('.aclamadasPor');
+    let cadaAgregada = document.querySelector('.cadaAgregada');
     let cadaTitulo = document.querySelector('.cadaTitulo');
 
     fetch('https://api.themoviedb.org/3/tv/popular?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
@@ -24,6 +26,51 @@ window.addEventListener('load', function(){
     .catch(function(error){
         console.log(error)
     })
+
+    fetch('https://api.themoviedb.org/3/tv/top_rated?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(seriesAclamadas){
+        console.log(seriesAclamadas)
+
+        seriesAclamadas.results.forEach(aclamada => {  
+            aclamadasPor.innerHTML += `
+                                        <a href='detalleSeries.html?id=${aclamada.id}&titulo=${aclamada.name}&overview=${aclamada.overview}&popularity=${aclamada.popularity}&portada=${aclamada.poster_path}&fecha=${aclamada.first_air_date}&votos=${aclamada.vote_average}&genero=${aclamada.genre_ids.id}'>
+                                            <div>
+                                                 <img src="https://image.tmdb.org/t/p/w500${aclamada.poster_path}" alt="${aclamada.name}"/>
+                                                <h2 class="cadaTitulo">${aclamada.name}</h2>
+                                            </div>
+                                        </a>
+                                    `
+        });
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
+    fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=18581b65b3e6ad002984aa4952878117&language=en-US&page=1')
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(agregadas){
+        console.log(agregadas)
+
+        agregadas.results.forEach(agregada => {  
+            cadaAgregada.innerHTML += `
+                                        <a href='detalleSeries.html?id=${agregada.id}&titulo=${agregada.name}&overview=${agregada.overview}&popularity=${agregada.popularity}&portada=${agregada.poster_path}&fecha=${agregada.first_air_date}&votos=${agregada.vote_average}&genero=${agregada.genre_ids.id}'>
+                                            <div>
+                                                 <img src="https://image.tmdb.org/t/p/w500${agregada.poster_path}" alt="${agregada.name}"/>
+                                                <h2 class="cadaTitulo">${agregada.name}</h2>
+                                            </div>
+                                        </a>
+                                    `
+        });
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
 
 
     
